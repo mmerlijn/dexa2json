6f03bf97825fd72b9abac21d1dfb56e2255e91c3
Menno <menno@mmerlijn.nl>
2021-07-13 09:59:24 +0200

    first release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='dexaExport2json',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/mmerlijn/dexa2json',
+    license='',
+    author='Menno',
+    author_email='menno@mmerlijn.nl',
+    description='Export dexa data to json'
+)
